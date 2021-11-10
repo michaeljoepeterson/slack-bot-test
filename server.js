c69f50dfb98acef945f4502218bfcb44f6e17bdb
michael peterson <michaeljoepeterson@gmail.com>
2021-11-09 18:59:58 -0700

    test express

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-//const express = require('express');
+const express = require('express');
 const { PORT,SLACK_BOT_TOKEN,SIGN_SECRET } = require('./config');
 //const { errorHandler } = require('./middleware/error-handler');
 //const { cors } = require('./middleware/cors');
@@ -9,19 +9,20 @@ const slackApp = new App({
   signingSecret: SIGN_SECRET,
   token: SLACK_BOT_TOKEN,
 })
-/*
 const app = express();
+/*
 
 app.use(express.json());
 app.use(cors);
 app.use(cors);
 */
 async function runServer() {
-  /*
-  server = app.listen(PORT, () => {
+  
+  app.listen(PORT, () => {
     console.log(`Your app is listening on port ${PORT}`);
   });
-  */
+  
+ /*
   try{
     await slackApp.start(PORT);
     console.log('app slack',PORT);
@@ -29,6 +30,7 @@ async function runServer() {
   catch(e){
     console.log(e);
   }
+  */
 } 
 
 runServer();
